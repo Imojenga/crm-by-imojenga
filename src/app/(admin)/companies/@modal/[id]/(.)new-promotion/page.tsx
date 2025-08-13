7fa2252517717b53f44d5f10585169b25d0a6b56
Imojenga <aina.jenko@gmail.com>
2025-08-13 23:55:46 +0300

    fix types

--- src/app/(admin)/companies/@modal/[id]/(.)new-promotion/page.tsx
+++ src/app/(admin)/companies/@modal/[id]/(.)new-promotion/page.tsx
@@ -1,18 +1,16 @@
 'use client';
 
 import React from 'react';
-import { useRouter } from 'next/navigation';
+import { useRouter, useParams } from 'next/navigation';
 import PromotionFormModal from '@/app/components/promotion-form-modal';
 
-export interface PageProps {
-  params: { id: string };
-}
-
-export default function Page({ params }: PageProps) {
+export default function Page() {
   const router = useRouter();
+  const { id } = useParams<{ id: string }>();
+
   return (
     <PromotionFormModal
-      companyId={params.id}
+      companyId={id}
       show={true}
       onClose={() => router.back()}
     />
--- src/app/(admin)/companies/[id]/new-promotion/page.tsx
+++ src/app/(admin)/companies/[id]/new-promotion/page.tsx
@@ -1,16 +1,15 @@
 'use client';
 
 import React from 'react';
+import { useParams } from 'next/navigation';
 import PromotionForm from '@/app/components/promotion-form';
 
-export interface PageProps {
-  params: { id: string };
-}
+export default function Page() {
+  const { id } = useParams<{ id: string }>();
 
-export default function Page({ params }: PageProps) {
   return (
     <div className="py-6 px-10">
-      <PromotionForm companyId={params.id} />
+      <PromotionForm companyId={id} />
     </div>
   );
 }
